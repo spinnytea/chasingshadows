24901db2af92c7113a384bc07d4f94b1bbac0f04
Garrett Tjarks <spinnytea@gmail.com>
2014-07-26 10:17:40 -0400

    deep extends!
    this is probably not the way that we want to create objects..

--- client/game.js
+++ client/game.js
@@ -1,10 +1,26 @@
 var _ = require('lodash');
 var angular = require('angular');
 
+angular.deepExtend = function(dst) {
+    angular.forEach(arguments, function(obj) {
+        angular.forEach(obj, function(value, key) {
+            if(!angular.isDefined(dst[key]))
+                dst[key] = angular.copy(value);
+            else if(angular.isObject(value))
+                angular.deepExtend(dst[key], value);
+            else
+                dst[key] = value;
+        });
+    });
+    return dst;
+}
+
 var app = angular.module('chasingshadows', []);
 
+app.value('angular', angular);
 app.factory('playerModel', require('./models/player'));
 app.factory('gameObjects', require('./models/gameObjects'));
+app.factory('object.base', require('./models/objects/base'));
 
 app.controller('rootController', require("./controllers/root"));
 app.controller('mapController', require("./controllers/map"));
--- client/models/player.js
+++ client/models/player.js
@@ -1,43 +1,22 @@
-module.exports = [function() {
-    var model = {
-
-        // the bounds will be managed by the server
-        // we will need to send and receive changes to the player's location
+module.exports = [
+'angular', 'object.base',
+function(angular, base) {
+    var model = angular.deepExtend({}, base, {
         bounds: {
-            // these values tell us where the player is located
             x: 10,
             y: 20,
-
-            // these probably won't change much
             width: 50,
             height: 50,
         },
-
-        // render is local data that we need to put the player on the map
         render: {
-            // these values need to be re computed when the bounds change
-            x: 0,
-            y: 0,
-            height: 0,
-            width: 0,
-
             offsets: {
-                x: 1,
-                width: -2,
                 y: -10,
                 height: 10,
-            },
+            }
+        }
+    });
 
-            calc: function() {
-                model.render.x = model.bounds.x + model.render.offsets.x;
-                model.render.y = model.bounds.y + model.render.offsets.y;
-                model.render.width = model.bounds.width + model.render.offsets.width;
-                model.render.height = model.bounds.height + model.render.offsets.height;
-            },
-        },
-    };
-
-    model.render.calc();
+    model.render.calc(model);
 
     return model;
 }];
