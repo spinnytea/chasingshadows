80cb48e5b1dbe95fa0d8feebaf4c06584ce1304d
Garrett Tjarks <spinnytea@gmail.com>
2014-07-26 12:45:36 -0400

    setup a demo for socket
    this needs to be changed and separated into different files, but at
    least it's a start

--- client/game.js
+++ client/game.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var angular = require('angular');
+var socket = require('socket.io-client')('http://localhost:3030');
 
 var app = angular.module('chasingshadows', []);
 
@@ -11,3 +12,8 @@ app.factory('object.player', require('./models/objects/player'));
 
 app.controller('rootController', require("./controllers/root"));
 app.controller('mapController', require("./controllers/map"));
+
+socket.on('connect', function() {
+    console.log('connected');
+});
+socket.emit('event', { key: "value" });
